222e7e95337c107758093d3d94c7fa9cb57ec167
João Victor Grisolia Luis <joao.vgl@puccampinas.edu.br>
2026-03-20 10:36:32 -0300

    integração das funções de votação e mesário

--- LAD.py
+++ LAD.py
@@ -1,7 +1,5 @@
 import os
 
-eleitores
-
 def gerenciamento():
     os.system('cls')
     print('''                                                                                                  
@@ -24,7 +22,51 @@ def gerenciamento():
         candidatos()
     elif n == 3:
         menu()
+
+def votacao():
+    os.system('cls')
+    print('''
+                                                    
+                                                                  
+,--.   ,--.       ,--.                                
+ \  `.'  /,---. ,-'  '-. ,--,--. ,---. ,--,--. ,---.  
+  \     /| .-. |'-.  .-'' ,-.  || .--'' ,-.  || .-. | 
+   \   / ' '-' '  |  |  \ '-'  |\ `--.\ '-'  |' '-' ' 
+    `-'   `---'   `--'   `--`--' `---' `--`--' `---'  
+                                                    ''')
+
+    n = input("\nPressione ENTER para abrir o mesário.\n")
+    mesario()
+
+def mesario():
+    os.system('cls')
+    print('''
+                                                            
+,--.   ,--.                             ,--.        
+|   `.'   | ,---.  ,---.  ,--,--.,--.--.`--' ,---.  
+|  |'.'|  || .-. :(  .-' ' ,-.  ||  .--',--.| .-. | 
+|  |   |  |\   --..-'  `)\ '-'  ||  |   |  |' '-' ' 
+`--'   `--' `----'`----'  `--`--'`--'   `--' `---'  
+                                                    ''')
+
+    print('''
+        Digite o valor da opção desejada:
+
+        1. Menu da Urna
+        2. Resultados
+        3. Auditoria
+        4. Sair
+    ''')
+    n = input("-> ")
+
+    if (n == 1):
+        menu_urna()
     
+    elif (n == 2):
+        resultados_votacao()
+
+    elif (n == 3):
+        auditoria_votacao()
     
 
 def menu():
@@ -33,6 +75,7 @@ def menu():
     
     print('''
             Digite a opção que deseja realizar: 
+
             1. Gerenciamento
             2. Votação
             3. Sair
