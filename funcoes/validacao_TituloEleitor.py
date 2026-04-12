6f1649ea8281c06a25fe1653539e57880a06d6cd
FelipeOS6 <felipe.os6@puccampinas.edu.br>
2026-04-12 16:49:26 -0300

    validação de titulo de eleitor

---
+++ funcoes/validacao_TituloEleitor.py
@@ -0,0 +1,44 @@
+def validar_titulo_eleitor(titulo):
+    titulo = titulo.strip() #remove espaços em branco no começo e no final da string. para nao ultrapassar os 12 digitos
+
+    # Verifica se tem 12 dígitos e se é número
+    if len(titulo) != 12 or not titulo.isdigit():  #verifica se tem 12 digitos e se é tudo número, se não for, retorna false
+        return False
+
+    # Separando partes
+    numero = titulo[:8] #pega indice do 0 ao 7
+    uf = titulo[8:10] #8 ao 9
+    dv_informado = titulo[10:] #10 ate o final
+
+    # 1º Dígito Verificador
+    soma1 = 0
+    peso = 2
+
+    for digito in numero:
+        soma1 += int(digito) * peso
+        peso += 1
+
+    resto1 = soma1 % 11
+
+    if resto1 == 10:
+        dv1 = 0
+    else:
+        dv1 = resto1
+
+    # 2º Dígito Verificador
+    soma2 = (int(uf[0]) * 7) + (int(uf[1]) * 8) + (dv1 * 9)
+
+    resto2 = soma2 % 11
+
+    if resto2 == 10:
+        dv2 = 0
+    else:
+        dv2 = resto2
+
+    dv_calculado = str(dv1) + str(dv2)
+
+    # Comparação final
+    if dv_calculado == dv_informado:
+        return True
+    else:
+        return False
