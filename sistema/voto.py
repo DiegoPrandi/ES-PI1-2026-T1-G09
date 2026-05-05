a0ab021fed26d226286727877e3031592284429f
Rafael França Cardoso <rafael.fc9@puccampinas.edu.br>
2026-05-05 18:00:39 -0300

    tirando o votou voto.py
    esqueci de tirar antes de commitar o commit anterior

--- sistema/voto.py
+++ sistema/voto.py
@@ -74,96 +74,92 @@ def adicionar_voto(eleitor_id, conn): # Criação de uma função com um parâme
 
     try:
         cursor = conn.cursor() # Cria um cursor para modificação no banco
-        votou = 0
         id_candidato = None
 
-        while votou == 0:
-            while not id_candidato:
-                voto = int(input("Digite o número de seu candidato: "))
-                cursor.execute('''
-                    SELECT id FROM candidatos WHERE numero_votacao = %s
-                            ''', (voto,)) # Seleciona o id do candidato em base do seu numero de voto
-                id_candidato = cursor.fetchone() # Atribui ao id_candidato a tupla 
+        while not id_candidato:
+            voto = int(input("Digite o número de seu candidato: "))
+            cursor.execute('''
+                SELECT id FROM candidatos WHERE numero_votacao = %s
+                        ''', (voto,)) # Seleciona o id do candidato em base do seu numero de voto
+            id_candidato = cursor.fetchone() # Atribui ao id_candidato a tupla 
 
-                if id_candidato: # Se a tupla id_candidato existir, ou seja, se o número do candidato for válido, extrai o valor do id do candidato da tupla e atribui à variável id_candidato
-                    id_candidato = id_candidato[0] # Extrai o valor id do candidato da tupla
-                else:
-                    print("Candidato inválido.")
-                    continue
+            if id_candidato: # Se a tupla id_candidato existir, ou seja, se o número do candidato for válido, extrai o valor do id do candidato da tupla e atribui à variável id_candidato
+                id_candidato = id_candidato[0] # Extrai o valor id do candidato da tupla
+            else:
+                print("Candidato inválido.")
+                continue
 
-            sql = 'SELECT nome_completo, numero_votacao, nome_partido, foto_ascii FROM candidatos WHERE numero_votacao = %s'
-            cursor.execute(sql,(voto,))
+        sql = 'SELECT nome_completo, numero_votacao, nome_partido, foto_ascii FROM candidatos WHERE numero_votacao = %s'
+        cursor.execute(sql,(voto,))
 
-            resultado = cursor.fetchone()
-            if resultado:
-                if resultado[3]:
-                    try:
-                        with open(resultado[3], 'r', encoding='utf-8') as f:
-                            print(f.read())
-                    except:
-                        print('Erro ao carregar ASCII')
-                else:
-                    print('Sem Imagem')
-                print(f"Nome: {resultado[0]}")
-                print(f"Número: {resultado[1]}")
-                print(f"Partido: {resultado[2]}")
+        resultado = cursor.fetchone()
+        if resultado:
+            if resultado[3]:
+                try:
+                    with open(resultado[3], 'r', encoding='utf-8') as f:
+                        print(f.read())
+                except:
+                    print('Erro ao carregar ASCII')
+            else:
+                print('Sem Imagem')
+            print(f"Nome: {resultado[0]}")
+            print(f"Número: {resultado[1]}")
+            print(f"Partido: {resultado[2]}")
 
-            n = str(input("Deseja confirmar seu voto no seguinte candidato? (S/N) -> ")).lower()
+        n = str(input("Deseja confirmar seu voto no seguinte candidato? (S/N) -> ")).lower()
 
-            while n != "s" and n != "n":
-                print("Digite apenas 's' ou 'n'")
-                n = input("Deseja confirmar seu voto? (S/N) -> ").lower()
+        while n != "s" and n != "n":
+            print("Digite apenas 's' ou 'n'")
+            n = input("Deseja confirmar seu voto? (S/N) -> ").lower()
 
-            if (n == "s"):
-                votou += 1
-                
-                data_atual = datetime.now() # Pega a data atual
+        if (n == "s"):
+            data_atual = datetime.now() # Pega a data atual
 
-                #Criação do protocolo de votação
-                letra1 = random.choice(string.ascii_uppercase) # Gera uma letra maiúscula aleatória
-                letra2 = random.choice(string.ascii_uppercase) # Gera uma letra maiúscula aleatória
-                num1 = random.choice (string.digits) # Gera um número aleatório
-                num2 = random.choice (string.digits) # Gera um número aleatório
-                num3 = random.choice (string.digits) # Gera um número aleatório
-                num4 = random.choice (string.digits) # Gera um número aleatório
-                num5 = random.choice (string.digits) # Gera um número aleatório
+            #Criação do protocolo de votação
+            letra1 = random.choice(string.ascii_uppercase) # Gera uma letra maiúscula aleatória
+            letra2 = random.choice(string.ascii_uppercase) # Gera uma letra maiúscula aleatória
+            num1 = random.choice (string.digits) # Gera um número aleatório
+            num2 = random.choice (string.digits) # Gera um número aleatório
+            num3 = random.choice (string.digits) # Gera um número aleatório
+            num4 = random.choice (string.digits) # Gera um número aleatório
+            num5 = random.choice (string.digits) # Gera um número aleatório
 
-                ano = "26" # Define o ano como 2026
-                numero_candidato = str(voto) # Converte o número do candidato para string   
+            ano = "26" # Define o ano como 2026
+            numero_candidato = str(voto) # Converte o número do candidato para string   
 
-                protocolo = (
-                    "V" +
-                    letra1 +
-                    letra2 +
-                    ano +
-                    numero_candidato +
-                    num1 +
-                    num2 +
-                    num3 +
-                    num4 +
-                    num5
-                        ) # Cria o protocolo de votação juntando as partes  
-                
-                protocolo_criptografado = criptografia(protocolo)
-                
-                cursor.execute('''
-                    INSERT into tabela_votos (id_candidato, data_hora_voto, protocolo_criptografado)
-                    VALUES (%s, %s, %s)
-                            ''', (id_candidato, data_atual, protocolo_criptografado)) # Insere no banco as informações de votação
-                
-                conn.commit() # Comita tudo
+            protocolo = (
+                "V" +
+                letra1 +
+                letra2 +
+                ano +
+                numero_candidato +
+                num1 +
+                num2 +
+                num3 +
+                num4 +
+                num5
+                    ) # Cria o protocolo de votação juntando as partes  
+            
+            protocolo_criptografado = criptografia(protocolo)
+            
+            cursor.execute('''
+                INSERT into tabela_votos (id_candidato, data_hora_voto, protocolo_criptografado)
+                VALUES (%s, %s, %s)
+                        ''', (id_candidato, data_atual, protocolo_criptografado)) # Insere no banco as informações de votação
+            
+            conn.commit() # Comita tudo
 
-                print("Voto registrado com sucesso!")
-                print("Protocolo de votação:", protocolo) # Mostra o protocolo de votação para o eleitor
+            print("Voto registrado com sucesso!")
+            print("Protocolo de votação:", protocolo) # Mostra o protocolo de votação para o eleitor
 
-                cursor.execute('''
-                    UPDATE eleitores SET status_voto = %s WHERE id = %s
-                            ''', (1, eleitor_id)) # Da update no status do eleitor
-                conn.commit() # Comita
+            cursor.execute('''
+                UPDATE eleitores SET status_voto = %s WHERE id = %s
+                        ''', (1, eleitor_id)) # Da update no status do eleitor
+            conn.commit() # Comita
 
-            elif (n == "n"):
-                n = input("Seu voto não será computado. Pressione ENTER para voltar.")
-                return adicionar_voto(eleitor_id, conn)
+        elif (n == "n"):
+            n = input("Seu voto não será computado. Pressione ENTER para voltar.")
+            return adicionar_voto(eleitor_id, conn)
         
     except mysql.connector.IntegrityError as e:
         print(f"\nErro ao votar: {e}") # Mostrando algum erro
