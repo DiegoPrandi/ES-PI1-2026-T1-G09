e7b3c3daa37e0ee4fba4815ae5e6d81b53f7c4d6
FelipeOS6 <felipe.os6@puccampinas.edu.br>
2026-05-06 11:16:50 -0300

    aplicado e modificado funcao de resultado da votacao, e aplicado descriptografia ao mostrar eleitor

--- sistema/eleitor.py
+++ sistema/eleitor.py
@@ -2,6 +2,7 @@ import os
 import menu.gerenciamento as gerenciamento
 from funcoes.chaveDeAcesso import gerar_chave_acesso
 import funcoes.criptografia as criptografia
+from funcoes.descriptografia import descriptografia
 from funcoes import ascii as ascii
 import mysql.connector
 from funcoes.criptografia import criptografia
@@ -74,6 +75,35 @@ def gestao_eleitores(conn):
                             print("Título de eleitor inválido. Tente novamente:")
                             titulo_eleitor = str(input("Digite seu título de eleitor: "))
                         print("Título válido!")
+
+                        cursor = conn.cursor()
+                        try:
+                            '''
+                                pra verificar se ja tem titulo,
+                                executa o sql pra ver o seguinte:
+                                "SELECT COUNT(*) FROM eleitores WHERE titulo_eleitor = %s", (titulo_eleitor,)
+                                ou seja
+                                quantas linhas tem na tabela eleitores com o titulo eleitor igual ao titulo que o cara digitou 
+
+                                se for 1 ou por algum motivo desconhecido maior que 1, entao ja tem um titulo igual no banco
+                            '''
+                            cursor.execute("SELECT COUNT(*) FROM eleitores WHERE titulo_eleitor = %s", (titulo_eleitor,))
+                            if cursor.fetchone()[0] > 0:
+                                '''
+                                entra no if, com isso oq o fetchone faz?
+                                resumindo pega o resultado que o banco retornou
+                                no caso o COUNT(*) contou da tabela eleitors quantos titulo iguais aquele que o cara digitou ele achou
+                                
+                                se o indice [0]  (oq o fetchone retorna) for maior que 0, entao ja existe no banco o titulo que a pessoa digitou
+                                retorna erro e volta pro gestao_eleitores
+                                '''
+                                print("\nTítulo de eleitor ja cadastrado")
+                                input("Pressione ENTER para voltar.")
+                                gestao_eleitores(conn)
+                                return
+                        finally:
+                            cursor.close()
+
                         n = 0
                         while n!=1 and n!=2:
                             print('''
@@ -160,7 +190,11 @@ def gestao_eleitores(conn):
                         print("Lista de eleitores:")
                         print('-' * 150)
                         for eleitor in result:
-                            print(f"Nome Completo: {eleitor[2]} | Chave de Acesso: {eleitor[1]} | Título de Eleitor: {eleitor[3]} | CPF Criptografado: {eleitor[4]} | Mesário: {eleitor[5]} | Status do Voto: {eleitor[6]}")
+                            cpf_A = str(eleitor[4])
+                            chaveA = str(eleitor[1])
+                            cpf_descriptografado = (descriptografia(cpf_A))
+                            chave_descriptografada = (descriptografia(chaveA))
+                            print(f"Nome Completo: {eleitor[2]} | Chave de Acesso: {chave_descriptografada} | Título de Eleitor: {eleitor[3]} | CPF Criptografado: {cpf_descriptografado} | Mesário: {eleitor[5]} | Status do Voto: {eleitor[6]}")
                         print('-' * 150)
 
                     listar_eleitores(conn)
