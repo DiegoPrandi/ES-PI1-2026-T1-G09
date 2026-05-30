206dfbc85f14b18a812aefc143000104a61763ce
Rafael França Cardoso <rafael.fc9@puccampinas.edu.br>
2026-05-30 20:17:07 -0300

    Update conexao.py

--- database/conexao.py
+++ database/conexao.py
@@ -17,8 +17,8 @@ def conectar():
             host='localhost',
             user='root',
             port=3306,
-            password='10100',
-            database='projetoPI',
+            password='root123@',
+            database='projetopi',
         )
 
         return conexao # se der tudo certinho retorna a conexão
